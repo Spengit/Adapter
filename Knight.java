fe92ae38b84a397d5f5152758dfdd133a2fb089a
Spengit <54564504+Spengit@users.noreply.github.com>
2019-10-23 14:54:56 -0400

    Add files via upload

---
+++ Knight.java
@@ -0,0 +1,11 @@
+
+public class Knight implements Warrior  {
+
+	public void attack() {
+		System.out.println("Attacking: I'm bringing you down");
+	}
+	
+	public void defend() {
+		System.out.println("Defending: we must protect the castle");
+	}
+}
---
+++ Warrior.java
@@ -0,0 +1,5 @@
+
+public interface Warrior {
+ public void attack();
+ public void defend();
+}
